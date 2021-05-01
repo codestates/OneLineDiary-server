dd79b9aa794ec7fc02744eaf8120946f310b5d2a
Lee Guen Hwa <rnjswjadhr09@gmail.com>
2021-05-01 15:54:30 +0900

    [CREATE] Edit User information and change branch

--- controller/mypage/editUserinfo.js
+++ controller/mypage/editUserinfo.js
@@ -2,14 +2,13 @@ const { user } = require("../../models");
 const { isAuthorized } = require("../tokenFunctions");
 
 module.exports = async (req, res) => {
-  //   console.log(req.body);
   const { userId, nickname, password } = req.body;
-  // req.body.password 에서 userinfo.dataValues 안의 기존 정보를 바꾼다.
+
   const userInfo = await user.findOne({ where: { userId } });
-  //   console.log(userInfo);
+
   if (password && userInfo) {
     userInfo.update({ password: password });
-    // console.log(userInfo);
+
     res.status(200).json({ message: "개인정보가 변경 되었습니다" });
   } else if (nickname && userInfo) {
     userInfo.update({ nickname: nickname });
